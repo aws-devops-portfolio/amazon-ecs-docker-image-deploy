c5a146496c9be0499e97fea92f3775bca2506c49
Mike <mikemonchu@gmail.com>
2026-03-09 23:56:55 +0200

    code refactoring

--- product-app/src/main/java/com/devhub164/product/controller/ProductController.java
+++ product-app/src/main/java/com/devhub164/product/controller/ProductController.java
@@ -1,7 +1,6 @@
 package com.devhub164.product.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +21,6 @@ public class ProductController {
     @GetMapping("/products")
     public List<String> getProducts(){
         return productService.getProducts();
-
     }
 
 }
